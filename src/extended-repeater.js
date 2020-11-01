7701454709e53916fc81f6989bf29bdbec6d00d4
yantomchik <55480251+YanTomchik@users.noreply.github.com>
2020-11-01 17:23:38 +0300

    init: add code

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, 
+  {
+    repeatTimes: strRep, 
+    separator: strSep, 
+    addition: add, 
+    additionRepeatTimes: addRep, 
+    additionSeparator: addSep 
+  }
+  ) {
+
+  if (add === undefined) add = ''
+  result = gen(add, addRep || 1, addSep || '|')
+  return gen(str, strRep || 1, strSep || '+', result)
 };
   
+function gen(str, max, separator1, separator2 = '') {
+  let result = ''
+  for (let i = 0; i < max; i++) {
+    if (i === max - 1) separator1 = ''
+    result += `${str}${separator2}${separator1}`
+  }
+  return result
+}
